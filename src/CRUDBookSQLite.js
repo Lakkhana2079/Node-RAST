899f8aeecacab07528534dbc32f0c34b91202366
Lakkhana2079 <lakkhana2079@gmail.com>
2023-09-12 09:39:25 +0700

    update

--- src/CRUDBookSQLite.js
+++ src/CRUDBookSQLite.js
@@ -71,5 +71,5 @@ app.delete('/books/:id', (req, res) => {
     });   
 });
 
-const port = process.env.PORT || 4500;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
