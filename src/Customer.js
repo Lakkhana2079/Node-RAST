1248606d5706f7ebb46c40a72029feafd46caa8e
Lakkhana2079 <lakkhana2079@gmail.com>
2023-09-29 13:40:50 +0700

    update

--- src/Customer.js
+++ src/Customer.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 const app = express();
 
-const db = new sqlite3.Database('./Database/Customer');
+const db = new sqlite3.Database('./Database/Customer.sqlite');
 
 app.use(express.json());
 
